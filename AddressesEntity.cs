e8156db948700ce20a2059a8559ac32d78d8eb08
igromanru <igromanru@yahoo.de>
2017-04-10 20:58:21 +0200

    Refactoring: is now 64 bit compatible

--- AddressesEntity.cs
+++ AddressesEntity.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace IgroGadgets
 {
@@ -26,11 +23,6 @@ namespace IgroGadgets
         {
             Address = address;
             Offsets = offsets;
-        }
-
-        public AddressEntity(int address, int[] offsets)
-            : this(new IntPtr(address), offsets)
-        {
         }
     }
 }
--- ProcessMemory.cs
+++ ProcessMemory.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
 using System.Runtime.InteropServices;
-using System.Text;
 
 namespace IgroGadgets
 {
@@ -34,61 +31,83 @@ namespace IgroGadgets
         public static extern IntPtr OpenProcess(int dwDesiredAccess, bool bInheritHandle, int dwProcessId);
 
         [DllImport("kernel32.dll", SetLastError = true)]
-        public static extern bool ReadProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, byte[] lpBuffer, int dwSize, IntPtr lpNumberOfBytesRead);
+        public static extern bool ReadProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, byte[] lpBuffer, UIntPtr dwSize, UIntPtr lpNumberOfBytesRead);
 
         [DllImport("kernel32.dll", SetLastError = true)]
-        public static extern bool WriteProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, byte[] lpBuffer, int dwSize, IntPtr lpNumberOfBytesWritten);
+        public static extern bool WriteProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, byte[] lpBuffer, UIntPtr dwSize, UIntPtr lpNumberOfBytesWritten);
 
         [DllImport("kernel32.dll", SetLastError = true)]
         public static extern bool CloseHandle(IntPtr handle);
 
-        private IntPtr handleProcess;
+        private IntPtr _handleProcess;
 
-        public ProcessMemory(String processName)
+        public ProcessMemory(string processName)
         {
             Process.EnterDebugMode();
             OpenProcessByName(processName);
         }
 
-        public IntPtr OpenProcessByName(String processName)
+        public IntPtr OpenProcessByName(string processName)
         {
             Process process = Process.GetProcessesByName(processName)[0];
-            handleProcess = OpenProcess((int)ProcessAccessType.PROCESS_ALL_ACCESS, false, process.Id);
-            return handleProcess;
+            _handleProcess = OpenProcess((int)ProcessAccessType.PROCESS_ALL_ACCESS, false, process.Id);
+            return _handleProcess;
         }
 
         public bool ReadMemory(IntPtr readAddress, ref byte[] readBuffer)
         {
-            return ReadProcessMemory(handleProcess, readAddress, readBuffer, readBuffer.Length, IntPtr.Zero);
+            return ReadProcessMemory(_handleProcess, readAddress, readBuffer, (UIntPtr)readBuffer.Length, UIntPtr.Zero);
         }
 
-        public int ReadMemoryByte(IntPtr readAddress)
+        public int ReadMemoryByte(IntPtr address)
         {
-            int readInt = -1;
-            byte[] readBuffer = new byte[sizeof(int)];
-            if (ReadProcessMemory(handleProcess, readAddress, readBuffer, 1, IntPtr.Zero))
+            int result = -1;
+            var readBuffer = new byte[sizeof(byte)];
+            if (ReadProcessMemory(_handleProcess, address, readBuffer, (UIntPtr)1, UIntPtr.Zero))
+            {
+                result = BitConverter.ToInt32(readBuffer, 0);
+            }
+            return result;
+        }
+
+        public IntPtr ReadMemoryIntPtr(IntPtr address)
+        {   
+            var result = IntPtr.Zero;
+            var readBuffer = new byte[IntPtr.Size];
+            if (ReadMemory(address, ref readBuffer))
+            {
+                Marshal.Copy(readBuffer, 0, result, readBuffer.Length);
+            }
+            return result;
+        }
+
+        public int ReadMemoryInt(IntPtr address)
+        {
+            var readInt = -1;
+            var readBuffer = new byte[sizeof(int)];
+            if (ReadMemory(address, ref readBuffer))
             {
                 readInt = BitConverter.ToInt32(readBuffer, 0);
             }
             return readInt;
         }
 
-        public int ReadMemoryInt(IntPtr readAddress)
+        public long ReadMemoryLong(IntPtr address)
         {
-            int readInt = -1;
-            byte[] readBuffer = new byte[sizeof(int)];
-            if (ReadMemory(readAddress, ref readBuffer))
+            long result = -1;
+            var readBuffer = new byte[sizeof(long)];
+            if (ReadMemory(address, ref readBuffer))
             {
-                readInt = BitConverter.ToInt32(readBuffer, 0);
+                result = BitConverter.ToInt64(readBuffer, 0);
             }
-            return readInt;
+            return result;
         }
 
-        public float ReadMemoryFloat(IntPtr readAddress)
+        public float ReadMemoryFloat(IntPtr address)
         {
-            float readFloat = -1f;
-            byte[] readBuffer = new byte[sizeof(int)];
-            if (ReadMemory(readAddress, ref readBuffer))
+            var readFloat = -1f;
+            var readBuffer = new byte[IntPtr.Size];
+            if (ReadMemory(address, ref readBuffer))
             {
                 readFloat = BitConverter.ToSingle(readBuffer, 0);
             }
@@ -97,58 +116,57 @@ namespace IgroGadgets
 
         public bool WriteMemory(IntPtr writeAddress, byte[] writeBuffer)
         {
-            return WriteProcessMemory(handleProcess, writeAddress, writeBuffer, writeBuffer.Length, IntPtr.Zero);
+            return WriteProcessMemory(_handleProcess, writeAddress, writeBuffer, (UIntPtr)writeBuffer.Length, UIntPtr.Zero);
         }
 
-        public bool WriteMemoryByte(IntPtr writeAddress, int writeByte)
+        public bool WriteMemoryByte(IntPtr address, int value)
         {
-            bool result = writeByte < 256;
-            byte[] write = BitConverter.GetBytes(writeByte);
-            if (result && WriteProcessMemory(handleProcess, writeAddress, write, 1, IntPtr.Zero))
-            {
-                result = true;
-            }
-            return result;
+            return value < 256 && WriteProcessMemory(_handleProcess, address, BitConverter.GetBytes(value), (UIntPtr)1, UIntPtr.Zero);
         }
 
-        public bool WriteMemoryInt(IntPtr writeAddress, int writeInt)
+        public bool WriteMemoryInt(IntPtr address, int value)
         {
-            byte[] write = BitConverter.GetBytes(writeInt);
-            return WriteMemory(writeAddress, write);
+            return WriteMemory(address, BitConverter.GetBytes(value));
         }
 
-        public bool WriteMemoryFloat(IntPtr writeAddress, float writeFloat)
+        public bool WriteMemoryLong(IntPtr address, long value)
         {
-            byte[] write = BitConverter.GetBytes(writeFloat);
-            return WriteMemory(writeAddress, write);
+            return WriteMemory(address, BitConverter.GetBytes(value));
+        }
+
+        public bool WriteMemoryFloat(IntPtr address, float value)
+        {
+            return WriteMemory(address, BitConverter.GetBytes(value));
         }
 
         // The pointer offets array have to looks like this: { 0xFB3E3C, 0x60, 0x8,...}
         // First offset makes the base address: 0x400000 + 0xFB3E3C = 0x13B3E3C
-        public IntPtr ReadPointer(int startAddress, int[] offsets)
+        public IntPtr ReadPointer(IntPtr address, int[] offsets)
         {
-            int pointedAddress = startAddress;
-            for (int i = 0; i < offsets.Length; i++)
+            for (var i = 0; i < offsets.Length; i++)
             {
                 if (i == offsets.Length - 1)
                 {
-                    pointedAddress += offsets[i];
+                    address = IntPtr.Add(address, offsets[i]);
                 }
-                else if ((pointedAddress = ReadMemoryInt(new IntPtr(pointedAddress + offsets[i]))) == -1)
+                else
                 {
-                    pointedAddress = 0;
-                    break;
+                    address = ReadMemoryIntPtr(IntPtr.Add(address, offsets[i]));
+                    if (address.Equals(IntPtr.Zero))
+                    {
+                        break;
+                    }
                 }
             }            
-            return new IntPtr(pointedAddress);
+            return address;
         }
 
         public void Close()
         {
             Process.LeaveDebugMode();
-            if(handleProcess != null)
+            if(!_handleProcess.Equals(IntPtr.Zero))
             {
-                CloseHandle(handleProcess);
+                CloseHandle(_handleProcess);
             }            
         }
     }
